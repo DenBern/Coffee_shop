9330b3b5f50952535e2d10cefdb0922fcd513445
Denis Bernovich <denisbernovich@gmail.com>
2023-01-30 22:32:50 +0300

    header

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <div className="header_main">
+            <div className="background">
+                <div className="navigation">
+                    <nav>
+                        <ul className="items">
+                            <li className="item"><img src="" alt="coffee_beans"/></li>
+                            <li className="item"><a href="">Coffee house</a></li>
+                            <li className="item"><a href="">Our coffee</a></li>
+                            <li className="item"><a href="">For your pleasure</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="desctiption_header">
+                    <h1></h1>
+                    <div className="">
+
+                    </div>
+                    <p className=""></p>
+                    <buttton type="button"></buttton>
+                </div>
+            </div>
+        </div>   
+    )
+}
