a5f3bb237be31d8c5377edb8f015a0d2f06a18f8
Bernovich Denis <denisbernovich@gmail.com>
2023-02-04 16:58:56 +0300

    fix our best

--- my-app/src/components/CoffeeItems/CoffeeItems.js
+++ my-app/src/components/CoffeeItems/CoffeeItems.js
@@ -4,7 +4,10 @@ export const CoffeeItems = ({coffee, img, price}) => {
     return (
         <li className="item_wrapper">
             <div className="item_coffee">
-                <img src={img} alt=""></img>
+                <div 
+                className='coffee_item_image'
+                style={{backgroundImage: `url(${img})`}}>
+                </div>
                 <div className="item_wrapper_name_price">
                     <p className="item_name">{coffee}</p>
                     <p className="item_price">{`${price}$`}</p>
